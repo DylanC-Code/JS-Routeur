af331b404c06965f7bf012018ad90642bf3b7346
DylanC-Code <dylan.castor06@gmail.com>
2023-01-15 18:18:27 +0100

    class Request implemented and manageParams method is working

--- dist/Request.js
+++ dist/Request.js
@@ -1,7 +1,27 @@
 export class Request {
     req;
-    constructor(req) {
+    route;
+    path;
+    params = {};
+    constructor(req, route) {
         this.req = req;
+        this.route = route;
+        this.path = req.url;
+        this.manageParams(route);
+    }
+    manageParams(url) {
+        const wrongParams = this.extractWrongParams(url);
+        const paramsName = url.match(/:([^/]+)/g)?.map((param) => param.slice(1));
+        let params = this.path?.match(/([^/]+)/g);
+        params = params.filter((param) => this.notInArray(wrongParams, param));
+        if (paramsName)
+            this.addParams(paramsName, params);
+    }
+    extractWrongParams = (url) => url.split("/").filter((group) => /^([\w_-]+)$/.test(group));
+    notInArray = (array, toFind) => array.find((element) => element === toFind);
+    addParams(paramsName, paramsValue) {
+        paramsName.forEach((name, index) => (this.params[name] = paramsValue[index]));
     }
 }
+new Request({ url: "/users/42" }, "/users/:id/:name");
 //# sourceMappingURL=Request.js.map
--- src/Request.ts
+++ src/Request.ts
@@ -1,5 +1,37 @@
 import { IncomingMessage } from "node:http";
+import { Route } from "./Route";
 
 export class Request {
-  constructor(private req: IncomingMessage) {}
+  path;
+  params: { [key: string]: string } = {};
+  constructor(private req: any, private route: string) {
+    this.path = req.url;
+    this.manageParams(route);
+  }
+
+  private manageParams(url: string) {
+    const wrongParams = this.extractWrongParams(url);
+    const paramsName = url.match(/:([^/]+)/g)?.map((param) => param.slice(1));
+
+    let params = this.path?.match(/([^/]+)/g);
+    params = params.filter((param: string) =>
+      this.notInArray(wrongParams, param)
+    );
+
+    if (paramsName) this.addParams(paramsName, params);
+  }
+
+  private extractWrongParams = (url: string) =>
+    url.split("/").filter((group: string) => /^([\w_-]+)$/.test(group));
+
+  private notInArray = (array: string[], toFind: string) =>
+    array.find((element) => element === toFind);
+
+  private addParams(paramsName: string[], paramsValue: string[]) {
+    paramsName.forEach(
+      (name, index) => (this.params[name] = paramsValue[index])
+    );
+  }
 }
+
+new Request({ url: "/users/42" }, "/users/:id/:name");
